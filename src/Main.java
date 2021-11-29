272cc4bd8a6f68901f33b6d6f637bd87bca54fd0
vicore6 <vicore6@gmail.com>
2021-11-29 21:33:33 +0300

    ok

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 800;
+        int divider = 20;
+        int mile = ticketprice / divider;
+        System.out.println(mile);
+    }
+}
